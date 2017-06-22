8e9a3213996ff6dfdd0a729df23cc77687bcaadf
Chavane Minto <chevy@MacBook-Pro-15.dhcp.thefacebook.com>
2017-06-22 16:33:06 -0700

    Added Pull to Refresh to scrollView

--- TumblrView/PhotosViewController.swift
+++ TumblrView/PhotosViewController.swift
@@ -19,11 +19,14 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
     {
         super.viewDidLoad()
         
-        //let myImageUrlString = "https://i.imgur.com/tGbaZCY.jpg"
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        // add refresh control to table view
+        postView.insertSubview(refreshControl, at: 0)
+
         
-        // AFNetworking extension to UIImageView that allows
-        // specifying a URL for the image
-        //postView.setImageWithURL(NSURL(string: myImageUrlString)!)
+        
         
 
         postView.dataSource = self
@@ -53,8 +56,36 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
         task.resume()
         
 
-
-        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default,    delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                // TODO: Get the posts and store in posts property
+                
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                
+                // TODO: Reload the table view
+                self.postView.reloadData()
+                refreshControl.endRefreshing()
+            }
+        }
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -84,6 +115,10 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         let vc = segue.destination as! PhotoDetailsViewController
@@ -102,6 +137,8 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
         
         vc.photoURL = URL(string: urlString)
     }
+    
+    
 
     /*
     // MARK: - Navigation
